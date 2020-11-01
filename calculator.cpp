aa815a37e3b03324c83a0b0d54862e14e2180005
Teriatkin <teriatkin.n@gmail.com>
2020-11-02 01:30:06 +0200

    first commit

--- calculator.cpp
+++ calculator.cpp
@@ -15,3 +15,21 @@ int Calculator::Mul (double a, double b)
     return a * b + 0.5;
 >>>>>>> e0ea21b (add a multiplication operation)
 }
+void check_pass (string password)
+{
+    string valid_pass = "qwerty123";
+    if (password == valid_pass) {
+        cout << "Доступ разрешен." << endl;
+    } else {
+        cout << "Неверный пароль!" << endl;
+    }
+}
+
+int main()
+{    
+    string user_pass;
+    cout << "Введите пароль: ";
+    getline (cin, user_pass);
+    check_pass (user_pass);
+    return 0;
+}
